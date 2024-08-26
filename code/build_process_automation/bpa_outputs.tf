e609c468c30e001a1b50bef9285b1679d605094a
Christian Lechner <lechnerc77@users.noreply.github.com>
2024-08-26 10:35:53 +0200

    chore: cleanup unsued files/naming devcontainer (#1)

--- code/build_process_automation/bpa_outputs.tf
+++
@@ -1,4 +0,0 @@
-output "url_build_process_automation" {
-  value       = btp_subaccount_subscription.build_process_automation.subscription_url
-  description = "Subscription URL for SAP Business Process Automation"
-}
